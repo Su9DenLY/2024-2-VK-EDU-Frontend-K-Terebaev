090dbd0841bb0a880ca76623e9ec59a168b2fe4f
suddenly <terebaev1234@gmail.com>
2024-09-26 17:25:46 +0300

    update vite config

--- simple-chat/vite.config.js
+++ simple-chat/vite.config.js
@@ -1,9 +1,14 @@
 export default {
     root: 'src',
+    base: '/2024-2-VK-EDU-Frontend-K-Terebaev/',
     server: {
         port: 3000,
         watch: {
             usePolling: true
         }
+    },
+    build: {
+        outDir: 'src/dist',
+        emptyOutDir: true,
     }
 }
